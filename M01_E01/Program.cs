5384a9d5bbf3f5015c4a51053949b1e62783e34f
PJSM <pjsm.lesi@gmail.com>
2024-04-28 22:36:39 +0100

    Add project files.

---
+++ M01_E01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility.SetUnicodeConsole();
+
+            double height, length, areaRec;
+
+            Utility.WriteMessage("Altura: ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            Utility.WriteMessage("Largura: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            areaRec = height * length;
+
+            Utility.WriteMessage($"Area: {height} * {length} = {areaRec}");
+
+            Utility.TerminateConsole();
+        }
+    }
+}
