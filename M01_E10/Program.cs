e0d2274f38e6da9fe30656994cbd8ad5483bfe65
PJSM <pjsm.lesi@gmail.com>
2024-04-29 16:25:29 +0100

    Added exercise 10.

---
+++ M01_E10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10. Escreva um programa que converta dólares americanos para euros (a taxa de câmbio é de 1,1579 USD para 1€).
+            Utility.SetUnicodeConsole();
+
+            double dollarValue, euroValue, convertRate = 1.1579;
+
+            Utility.WriteMessage("Valor (USD): ");
+            dollarValue = double.Parse(Console.ReadLine());
+
+            euroValue = dollarValue / convertRate;
+
+            Utility.WriteMessage($"{dollarValue}USD <=> {euroValue:F2}€");
+
+            Utility.TerminateConsole();
+        }
+    }
+}
