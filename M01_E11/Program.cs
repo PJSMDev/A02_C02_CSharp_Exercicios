1a1fd126f75b7cd6b5839a2bcebbf3d7b4e3b856
PJSM <pjsm.lesi@gmail.com>
2024-04-29 17:01:33 +0100

    Added exercises 11 and 12.

---
+++ M01_E11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * 11. Escreva um programa que converta a temperatura lida em graus Fahrenheit para graus Celsius. 
+             *  A fórmula é C = 5/9 *(f - 32)
+            */
+            Utility.SetUnicodeConsole();
+
+            double fahrTemp, celsTemp;
+
+            Utility.WriteMessage("Valor em Fahrenheit: ");
+            fahrTemp = Convert.ToDouble(Console.ReadLine());
+
+            celsTemp = (double)5 / 9 * (fahrTemp - 32);     // para evitar truncar o 5/9
+
+            Utility.WriteMessage($"{fahrTemp} Fahrenheit <=> {celsTemp:F2} Celsius");
+
+            Utility.TerminateConsole();
+        }
+    }
+}
---
+++ M01_E12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 12. Escrever um programa que leia o preço base de um determinado produto e calcule o seu valor de venda ao 
+             *  público (ou seja, valor acrescido da taxa de IVA a 23%).
+            */
+            Utility.SetUnicodeConsole();
+
+            double priceBase, priceFinal, rateIVA = 1.23;
+
+            Utility.WriteMessage("Preço base: ");
+            priceBase = double.Parse(Console.ReadLine());
+
+            priceFinal = priceBase * rateIVA;
+
+            Utility.WriteMessage($"Preço base: {priceBase}\nPreço final: {priceFinal:F2}");
+
+            Utility.TerminateConsole();
+        }
+    }
+}
