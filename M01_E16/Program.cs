1e5a37c043ca295321f07457e6c41ae69ebf035f
PJSM <pjsm.lesi@gmail.com>
2024-05-02 15:31:03 +0100

    Added to Module 1: E16 (completed), E17 (unfinished)

---
+++ M01_E16/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 6. Escrever um programa que calcule a despesa média diária que um turista despendeu numa viagem de quatro 
+             *   dias ao Porto, sabendo que cada dia gastou mais 20% do que no dia anterior. O valor da despesa deverá ser 
+             *   apresentado sem casas decimais
+            */
+            Utility.SetUnicodeConsole();
+
+            double initialExp, totalExp, avgExp;
+
+            Utility.WriteMessage("Despesa inicial: ");
+            initialExp = Convert.ToDouble(Console.ReadLine());
+
+            totalExp = initialExp;
+
+            for (int i = 1; i < 4; i++)     // Começa no 2º dia
+            {
+                initialExp *= 1.20;
+                totalExp += initialExp;
+            }
+
+            // Calculando a despesa média diária
+            avgExp = totalExp / 4;
+
+            // Exibindo o resultado sem casas decimais
+            Utility.WriteMessage($"Despesa média diária: {Convert.ToInt16(avgExp)}", "", "\n");
+
+            Utility.TerminateConsole();
+        }
+    }
+}
---
+++ M01_E17/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace M01_E17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
